8aff9380c4a309bda3d87acf96042265d4fef6fb
Sam Blissard <samblissard@gmail.com>
2023-01-31 21:12:25 -0600

    Create ApiStack with get and post routes

--- stacks/index.js
+++ stacks/index.js
@@ -1,4 +1,5 @@
 import { App } from "@serverless-stack/resources";
+import { ApiStack } from "./ApiStack";
 import { StorageStack } from "./StorageStack";
 /**
  * @param {App} app
@@ -11,5 +12,5 @@ export default function (app) {
       format: "esm",
     },
   });
-  app.stack(StorageStack);
+  app.stack(StorageStack).stack(ApiStack);
 }
